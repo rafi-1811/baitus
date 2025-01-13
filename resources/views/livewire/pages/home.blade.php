2b6fbf661a810d9e86ea6dff41d7e39fefde5a3e
sadamhakiki <sadamhakiki0@gmail.com>
2025-01-13 12:57:27 +0700

    fix(tentang kami): page

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -156,46 +156,58 @@
     <!-- Section Recent Berita -->
     <section class="h21_blog-area pt-105 pb-140">
         <div class="container">
+            <!-- Judul Utama -->
             <div class="row justify-content-center">
-                <div class="col-12">
+                <div class="col-xl-5 col-xl-6 col-lg-6 col-md-8">
                     <div class="section-area text-center mb-50">
-                        <span class="section-subtitle tp_fade_left">bahagia Bersama Yatim</span>
+                        <span class="section-subtitle tp_fade_left">Bahagia Bersama Yatim</span>
                         <h2 class="section-title tp_title_slideup mb-0">Berita Terbaru</h2>
                     </div>
                 </div>
             </div>
+
+            <!-- Tombol Lihat Semua -->
             <div class="row">
                 <div class="col-12">
                     <div class="section-area text-end mb-30">
-                        <h6 style=""
-                            class="tp_fade_left d-flex justify-content-end align-items-center d-flex gap-2">
-                            <a wire:navigate href="{{ route('berita') }}">
-                                Lihat semua
+                        <h6 class="tp_fade_left d-flex justify-content-end align-items-center gap-2">
+                            <a wire:navigate href="{{ route('berita') }}" class="text-decoration-none text-dark">
+                                Lihat Semua
                             </a>
                             <i class="fa-solid fa-arrow-right"></i>
                         </h6>
-
                     </div>
                 </div>
             </div>
+
+            <!-- Daftar Berita -->
             <div class="row">
                 @forelse ($staticData['berita'] as $item)
-                    <div wire:key="{{ $item->slug }}" class="col-xl-4 col-md-6 tp_fade_left">
+                    <div wire:key="{{ $item->slug }}" class="col-xl-4 col-md-6 col-sm-12">
                         <div class="h2_blog-item mb-35">
                             <div class="h2_blog-img w_img mb-25">
-                                <a wire:navigate href="{{ route('detail-berita', ['slug' => $item->slug]) }}"><img
-                                        src="{{ asset('storage/' . $item->cover_gambar_berita) }}"
-                                        alt="{{ $item->program->kategori_program }}"></a>
+                                <a wire:navigate href="{{ route('detail-berita', ['slug' => $item->slug]) }}">
+                                    <img src="{{ asset('storage/' . $item->cover_gambar_berita) }}"
+                                        alt="{{ $item->program->kategori_program }}" class="img-fluid">
+                                </a>
                             </div>
                             <div class="h2_blog-content">
                                 <div class="h2_blog-content-meta">
-                                    <span><a href="#">{{ $item->program->kategori_program }}</a></span>
-                                    <span><i
-                                            class="fa-light fa-calendar-days"></i>{{ \Carbon\Carbon::parse($item->created_at)->locale('id')->format('d F Y') }}</span>
+                                    <span>
+                                        <a href="#" class="text-decoration-none text-muted">
+                                            {{ $item->program->kategori_program }}
+                                        </a>
+                                    </span>
+                                    <span class="ms-3">
+                                        <i class="fa-light fa-calendar-days"></i>
+                                        {{ \Carbon\Carbon::parse($item->created_at)->locale('id')->format('d F Y') }}
+                                    </span>
                                 </div>
-                                <h4 class="h2_blog-content-title">
-                                    <a wire:navigate
-                                        href="{{ route('detail-berita', ['slug' => $item->slug]) }}">{{ $item->judul }}.</a>
+                                <h4 class="h2_blog-content-title mt-3">
+                                    <a wire:navigate href="{{ route('detail-berita', ['slug' => $item->slug]) }}"
+                                        class="text-decoration-none text-dark">
+                                        {{ $item->judul }}
+                                    </a>
                                 </h4>
                             </div>
                         </div>
@@ -206,6 +218,7 @@
             </div>
         </div>
     </section>
+
 </main>
 
 
